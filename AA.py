b435b770c0f273ffa036b5d22a19f3a1f58577bf
celestial_evermore <46949994+celestialevermore@users.noreply.github.com>
2021-09-29 02:09:16 +0900

    Add files via upload

---
+++ AA.py
@@ -0,0 +1,35 @@
+
+N = int(input())
+res=[]
+
+
+arr = input().split()
+
+#print(arr)
+
+def digit_sum(x):
+    total=int(0)
+
+    for i in range(len(x)):
+        total+=int(x[i])
+    return total
+
+#print(digit_sum(arr[0]))
+
+
+for i in arr:
+    res.append(digit_sum(i))
+
+curmax=-1
+
+for i in range(len(res)):
+    if curmax <=res[i]:
+        curmax = res[i]
+
+Key=0
+for key,value in enumerate(res):
+    if curmax == value:
+        Key=key
+        break
+
+print(arr[Key])
